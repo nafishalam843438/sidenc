293e15764c62c7bf2644b64a6c66193cb6c14be8
nafishalam843438 <uniqueshorts580@gmail.com>
2024-12-30 22:30:23 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8066216378:AAH20METU25McUevqtsKaFiSKCuGI0H5zR8")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@IGNITE_OWNER_1")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5579438195")
+
+
+
+
+
+
+
+  
